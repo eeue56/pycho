75e51424cdae631eceed3d8b86128ed1a92563ad
eeue56 <eeue56@bangor.ac.uk>
2014-02-16 17:44:53 +0000

    updating setup..

--- setup.py
+++ setup.py
@@ -4,11 +4,24 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+try:
+    license = open('LICENSE').read()
+except:
+    license = None
+
+try:
+    readme = open('README.md').read()
+except:
+    readme = None
+
 setup(name='pycho',
       version='0.0.1',
       description='A game engine for Python 2 and 3 with built in AI',
+      long_description=readme,
+      license=license,
+      url='http://github.com/eeue56/pycho/',
       author='Enalicho',
       author_email='enalicho@gmail.com',
       packages=find_packages(),
       requires=['PyQt', 'OpenGL'],
-     )
+)
