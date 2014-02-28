b9ee789ecf657a731054bf465130ec007403fc01
eeue56 <eeue56@bangor.ac.uk>
2014-02-28 18:51:48 +0000

    working on helper methods for #22 and #14

--- pycho/gl/drawing.py
+++ pycho/gl/drawing.py
@@ -8,8 +8,8 @@ def draw_square(x, y, x_size=1, y_size=1):
     """ Draw a square of given location and size """
     gl.glRectf(x, y, x + x_size, y + y_size)
 
-def generate_rectangle_vbo(x, y, width=1, height=1):
-    return VBO(array([
+def rectangle_coords(x, y, width=1, height=1):
+    return [
         [x, y, 0.0],
         [x + width, y, 0.0],
         [x, y + height, 0.0],
@@ -17,7 +17,62 @@ def generate_rectangle_vbo(x, y, width=1, height=1):
         [x + width, y + height, 0.0],
         [x, y + height, 0.0],
         [x + width, y, 0.0]
-    ], dtype=float32))
+    ]
+
+def color_rectangle_coords(x, y, width=1, height=1, color=(1, 0, 0)):
+    r, g, b = color
+    return [
+        [x, y, 0.0, r, g, b],
+        [x + width, y, 0.0, r, g, b],
+        [x, y + height, 0.0, r, g, b],
+        
+        [x + width, y + height, 0.0, r, g, b],
+        [x, y + height, 0.0, r, g, b],
+        [x + width, y, 0.0, r, g, b]
+    ]
+
+def generate_rectangle_vbo(x, y, width=1, height=1):
+    return VBO(array(rectangle_coords(x, y, width, height), dtype=float32))
+
+def sections_into_coords(sections):
+    out = []
+
+    for (x, y, width, height, color) in sections:
+        out.extend(color_rectangle_coords(x, 
+            y, 
+            width, 
+            height, 
+            color))
+
+    return out
+
+def sectioned_vbo(sections):
+    points = sections_into_coords(sections)
+    return (VBO(array(points, dtype=float32)), len(points))
+
+def draw_color_vbo(vbo, offset=0, number=6):
+    try:
+        vbo.bind()
+
+        try:
+            gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+            gl.glEnableClientState(gl.GL_COLOR_ARRAY)
+
+            gl.glVertexPointerf(3, gl.GL_FLOAT, 24, vbo)
+            gl.glColorPointer(3, gl.GL_FLOAT, 24, vbo + 12)
+
+            gl.glDrawArrays(gl.GL_TRIANGLES, offset, number, None)
+
+        except Exception:
+            logging.error('failed to draw')
+        finally:
+            vbo.unbind()
+            gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+            gl.glDisableClientState(gl.GL_COLOR_ARRAY)
+    except Exception:
+        logging.error('failed to bind')
+    finally:
+        pass
 
 def draw_vbo(vbo, offset=0, number=6):
     try:
