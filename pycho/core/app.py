4db694e6187328504ba54be7fe17c9f6b07e54b9
eeue56 <eeue56@bangor.ac.uk>
2014-02-28 14:51:21 +0000

    fixed #11 by moving things out into a function

---
+++ pycho/core/app.py
@@ -0,0 +1,51 @@
+from pycho.gui.application import Application as App
+
+from pycho.world import World
+from pycho.core.game import Game
+from pycho.gui.windows import DefaultWindow
+
+from pycho.world_objects import Player
+from pycho.levels import LevelController, generate_objects
+
+from pycho.gl.color import COLORS
+from pycho.world.navigation import DIRECTIONS
+
+import logging
+import sys
+
+
+
+def application(level_file, 
+    player_x, 
+    player_y, 
+    player_color=None, 
+    player_direction=None, 
+    player_options=None):
+
+    logging.getLogger().setLevel(logging.ERROR)
+
+    if player_options is None:
+        player_options = {}
+
+    if player_color is None:
+        player_color = COLORS['white']
+
+    if player_direction is None:
+        player_direction = DIRECTIONS['up']
+
+
+    player = Player(player_x, player_y, 
+        player_color, player_direction, **player_options)
+
+    with open(level_file) as f:
+        levels = generate_objects(f.read())
+
+    level_controller = LevelController(levels)
+    world = World(player, level_controller)
+    game = Game(player, world)
+
+    app = App(sys.argv)
+    window = DefaultWindow(game)
+    window.show()
+
+    return (game, app, window)
--- pycho/main.py
+++ pycho/main.py
@@ -1,32 +1,5 @@
-from pycho.world import World
-from pycho.core.game import Game
-from pycho.gui.windows import DefaultWindow
-from pycho.gui.application import Application
-
-from pycho.world_objects import Player
-from pycho.levels import LevelController, generate_objects
-
-from pycho.gl.color import COLORS
-from pycho.world.navigation import DIRECTIONS
+from pycho.core.app import application
  
-
 if __name__ == '__main__':
-    import sys
-    import logging
-
-    logging.getLogger().setLevel(logging.ERROR)
-
-    player = Player(50, 50, COLORS['white'], DIRECTIONS['up'], speed=2)
-
-
-    with open('level_data.json') as f:
-        levels = generate_objects(f.read())
-
-    level_controller = LevelController(levels)
-    world = World(player, level_controller)
-    game = Game(player, world)
-
-    app = Application(sys.argv)
-    window = DefaultWindow(game)
-    window.show()
+    game, app, window = application('level_data.json', 50, 50, player_options={'speed' : 2})
     app.exec_()
